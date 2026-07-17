85d0c1990c1b9e1e09e86d08468f9085745bef61
OCahungo <OCahungo>
2026-07-17 11:50:23 +0000

    changes in the prints

--- Scripts/00_setup_environment.py
+++ Scripts/00_setup_environment.py
@@ -10,14 +10,14 @@ spark.sql("CREATE CATALOG IF NOT EXISTS bronze")
 spark.sql("CREATE CATALOG IF NOT EXISTS silver")
 spark.sql("CREATE CATALOG IF NOT EXISTS gold")
 
-print("✅ Catalogs created: bronze, silver, gold")
+print(" Catalogs created: bronze, silver, gold")
 
 spark.sql("CREATE SCHEMA IF NOT EXISTS bronze.superstore")
 spark.sql("CREATE SCHEMA IF NOT EXISTS silver.superstore")
 spark.sql("CREATE SCHEMA IF NOT EXISTS gold.superstore")
 
-print("✅ Schemas created: superstore inside each catalog")
+print(" Schemas created: superstore inside each catalog")
 
 spark.sql("CREATE VOLUME IF NOT EXISTS bronze.superstore.raw_superstore")
 
-print("✅ Volume created: bronze.superstore.raw_superstore")
+print(" Volume created: bronze.superstore.raw_superstore")
